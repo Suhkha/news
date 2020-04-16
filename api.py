f8ecab5f4f46e56e3cfda6a29e6652e33496d1a6
Jessica Villa <jessicavilla@Jessicas-MacBook-Air.local>
2020-04-15 19:07:37 -0500

    scraping news based on query search on google

--- api.py
+++ api.py
@@ -1,13 +1,47 @@
-from flask import Flask, jsonify, request
-import logging
+from flask import Flask, jsonify
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import nltk
 
 app = Flask(__name__)
+# desktop user-agent
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
-list_of_keywords = { "keywords": [ "china", "covid", "mexico" ] }
+list_of_keywords = { "keywords": [ 
+  "china", 
+  "covid", 
+  "mexico", 
+  "hospitales" 
+]}
 
-@app.route('/api/keywords', methods=['GET'])
-def all_keywords():
-  return jsonify(list_of_keywords)
+keywords = list_of_keywords.get('keywords')
+
+@app.route('/api/get_news', methods=['GET'])
+def get_news():
+  results = []
+  for keyword in keywords:
+    URL = "https://google.com/search?q="+keyword
+    headers = {"user-agent": USER_AGENT}
+    search = requests.get(URL, headers=headers)
+
+    if search.status_code == 200:
+      response = BeautifulSoup(search.content, "html.parser")
+      for new in response.find_all('div', class_='r'):
+        anchors = new.find_all('a')
+        if anchors:
+          link = anchors[0]['href']
+          title = new.find('h3').text
+          news = {
+            "keyword": keyword,
+            "content": title,
+            "reference": link,
+            "ranking" : freqdist1
+          }
+          results.append(news)
+          
+  return jsonify(results)
+
 
 @app.route('/api/new_keyword', methods=['POST'])
 def add_keyword():
